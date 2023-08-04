dcaf7b5fc2abbac9f4156b39230734357196b0f7
https://pokemon-infinite-fusion.coderobo.org/ <141412731+game-website@users.noreply.github.com>
2023-08-04 17:17:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+//document.onload = function() {
+
+console.log('Coderobo loading the app');
+
+// --- get all elements
+const mainCanvas = document.getElementById('mainCanvas');
+const mainCtx = mainCanvas.getContext('2d');
+
+const uploadDiv = document.getElementById('uploadDiv');
+    const uploadButton = document.getElementById('uploadButton');
+
+const exportDiv = document.getElementById('exportDiv');
+    const continueEditing = document.getElementById('continueEditing');
+    const exportEl = document.getElementById('exportEl');
